9f0d61d657843d01a5f91d1c359ae96533e67a5b
Varun Upadhyay <varun.u28@gmail.com>
2016-07-25 21:32:23 +0530

    Create Lario and Muigi Pipe Problem.py

---
+++ Python/Lario and Muigi Pipe Problem.py
@@ -0,0 +1,34 @@
+#Lario and Muigi Pipe Problem
+#Level : 8kyu
+'''
+Problem statement: Looks like some hoodlum plumber and his brother has been running around and 
+damaging your stages again.
+
+The pipes connecting your level's stages together need to be fixed before you recieve any more 
+complaints. Each pipe should be connecting, since the levels ascend, you can assume every number 
+in the sequence after the first index will be greater than the previous and that there will be no 
+duplicates.
+
+Task
+
+Given the a list of numbers, return the list so that the values increment by 1 for each index up 
+to the maximum value.
+
+Example:
+
+Input: 1,3,5,6,7,8
+
+Output: 1,2,3,4,5,6,7,8
+'''
+
+def pipe_fix(a):
+	lower = a[0]
+	upper = a[-1]
+	ans = []
+	while lower <= upper:
+		ans.append(lower)
+		lower+=1
+	return ans
+
+a = [1,2,3,5,6,8,9]
+print(pipe_fix(a))
