20ce4d2dbf80e1ff38660154d11040491144ca6f
Varun Upadhyay <varun.u28@gmail.com>
2016-11-18 08:28:55 +0530

    Create Keep The Order.py

---
+++ Python/Keep The Order.py
@@ -0,0 +1,30 @@
+# Keep the Order
+# Level: 7 kyu
+'''
+Problem Description:
+Your job here is to write a function (keepOrder in JS/CoffeeScript,
+keep_order in Ruby/Crystal/Python), which takes a sorted array ary and a
+value val, and returns the lowest index where you could insert val to
+maintain the sorted-ness of the array. The input array will always be sorted
+in ascending order. It may contain duplicates.
+
+Do not modify the input.
+Some examples:
+
+keep_order([1, 2, 3, 4, 7], 5) #=> 4
+                      ^(index 4)
+keep_order([1, 2, 3, 4, 7], 0) #=> 0
+          ^(index 0)
+keep_order([1, 1, 2, 2, 2], 2) #=> 2
+                ^(index 2)
+'''
+def keep_order(ary, val):
+    i = 0
+    while i < len(ary):
+        if ary[i] >= val:
+            break
+        i += 1
+    return i
+print(keep_order([1, 2, 3, 4, 7], 5))
+print(keep_order([1, 2, 3, 4, 7], 0))
+print(keep_order([1, 1, 2, 2, 2], 2))
