6be47c30c38b2c3cbd8291def282c660f051e12f
Varun Upadhyay <varun.u28@gmail.com>
2016-11-29 08:26:48 +0530

    Solved a 7 kyu Python Kata

---
+++ Python/Is every value in the array an array?.py
@@ -0,0 +1,28 @@
+# Is every value in the array an array?
+# Level : 7kyu
+'''
+Problem Description: Is every value in the array an array?
+
+This should only test the second array dimension of the array. The values of the nested arrays don't have 
+to be arrays.
+
+Examples:
+
+[[1],[2]] => true
+['1','2'] => false
+[{1:1},{2:2}] => false
+'''
+
+def arr_check(arr):
+	for i in arr:
+		if isinstance(i, list) == False:
+			return False
+	return True
+
+# Test Case
+
+print(arr_check([]))
+print(arr_check([['string']]))
+print(arr_check([[], {}]))
+print(arr_check([[1], [2], [3]]))
+print(arr_check(["A", "R", "R", "A", "Y"]))
--- README.md
+++ README.md
@@ -100,5 +100,6 @@ problems I solve here and I aim to continue this routine for next 180 days. I wi
 20. Ones and Zeros
 21. Nova polynomial add
 22. Find the missing letter
+23. Is every value in the array an array?
 
 My CodeWars profile: http://www.codewars.com/users/varunu28
