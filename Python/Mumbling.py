14d2de887a3d2094c899710d2bb72e8aa50e9865
João Nicoli <55551413+Joaonic@users.noreply.github.com>
2020-07-14 13:23:32 -0300

    Update Mumbling.py
    More pythonic way to do it.

--- Python/Mumbling.py
+++ Python/Mumbling.py
@@ -11,16 +11,12 @@ accum("cwAt")    # "C-Ww-Aaa-Tttt"
 def accum(s):
     # your code
     ans = ''
-    i = 0
-    while i < len(s):
-    	n = 0
-    	while n < i+1:
+    for i in range(len(s)):
+    	for n in range(i+1):
     		if n == 0:
     			ans += s[i].upper()
     		else:
     			ans += s[i].lower()
-    		n += 1
     	ans += '-'
-    	i += 1
     return ans[:len(ans)-1]
 
